49f599e55239c8f7dcc070971c3090cac819332d
王宇轩 <vincent.yuxuan.wang@gmail.com>
2018-03-03 22:27:24 +0800

    排序器调用方法

--- src/main/java/basic/OrderingExplained.java
+++ src/main/java/basic/OrderingExplained.java
@@ -37,9 +37,36 @@ public class OrderingExplained {
         System.out.println("usingToStringOrdering:"+ usingToStringOrdering.sortedCopy(list));
     }
 
+    /**
+     * 链式调用方法
+     * 通过链式调用，可以由给定的排序器衍生出其它排序器
+     */
+    public static void chainingMethod(){
+        List<String> list = Lists.newArrayList();
+        list.add("peida");
+        list.add("jerry");
+        list.add("harry");
+        list.add("eva");
+        list.add("jhon");
+        list.add(null);
+        System.out.println("list:"+ list);
+
+        // 对可排序类型做自然排序，如数字按大小，日期按先后排序
+        Ordering<String> naturalOrdering = natural();
+        // 获取语义相反的排序器
+        Ordering<String> reverseOrdering = natural().reverse();
+        // 使用当前排序器，但额外把null值排到最前面
+        natural().nullsFirst();
+        // 使用当前排序器，但额外把null值排到最后面
+        natural().nullsFirst();
+
+        System.out.println("naturalOrdering:"+ naturalOrdering.sortedCopy(list));
+        System.out.println("reverseOrdering:"+ reverseOrdering.sortedCopy(list));
+    }
 
     public static void main(String args[]) {
         creationMethod();
+        chainingMethod();
 
         Ordering<List<String>> ordering = new Ordering<List<String>>() {
             @Override
