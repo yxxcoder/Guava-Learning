ea3a2fed87c2d053ab8c07d3bfa0f675176ea566
王宇轩 <vincent.yuxuan.wang@gmail.com>
2018-03-28 23:54:55 +0800

    新集合类型

--- src/main/java/collections/newcollection/NewCollectionTypes.java
+++ src/main/java/collections/newcollection/NewCollectionTypes.java
@@ -181,6 +181,14 @@ public class NewCollectionTypes {
 
     }
 
+    /**
+     * RangeSet描述了一组不相连的、非空的区间
+     * 当把一个区间添加到可变的RangeSet时，所有相连的区间会被合并，空区间会被忽略
+     */
+    public static void rangeSet() {
+
+    }
+
     public static void main(String args[]) {
         multiset();
         multimap();
