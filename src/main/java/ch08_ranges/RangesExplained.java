a1fe105540d9dae608da19c399b2e070c2e47cbb
王宇轩 <vincent.yuxuan.wang@gmail.com>
2018-06-13 00:40:45 +0800

    区间运算

--- src/main/java/ch08_ranges/RangesExplained.java
+++ src/main/java/ch08_ranges/RangesExplained.java
@@ -1,5 +1,9 @@
 package ch08_ranges;
 
+import com.google.common.collect.BoundType;
+import com.google.common.collect.Range;
+import com.google.common.primitives.Ints;
+
 /**
  * 区间
  *
@@ -7,4 +11,161 @@ package ch08_ranges;
  * @create 2018-06-07 下午11:41
  **/
 public class RangesExplained {
+    public static void main(String[] args) {
+        // 构建区间
+        buildingRanges();
+        // 区间运算
+        operations();
+    }
+
+
+    /**
+     * 构建区间
+     * Range type	    Method
+     * (a..b)	        open(C, C)
+     * [a..b]	        closed(C, C)
+     * [a..b)	        closedOpen(C, C)
+     * (a..b]	        openClosed(C, C)
+     * (a..+∞)	        greaterThan(C)
+     * [a..+∞)	        atLeast(C)
+     * (-∞..b)	        lessThan(C)
+     * (-∞..b]	        atMost(C)
+     * (-∞..+∞)	        all()
+     */
+    private static void buildingRanges() {
+
+        // 字典序在"left"和"right"之间的字符串，闭区间
+        Range<String> closedLeftAndRight = Range.closed("left", "right");
+        // [left..right]
+        println(closedLeftAndRight.toString());
+
+        // 严格小于4.0的double值
+        Range<Double> lessThan4 = Range.lessThan(4.0);
+        // (-∞..4.0)
+        println(lessThan4);
+
+
+        /**
+         * 明确地指定边界类型来构造区间
+         * Range type	                    Method
+         * 有界区间	                        range(C, BoundType, C, BoundType)
+         * 有界区间 ((a..+∞) or [a..+∞))	    downTo(C, BoundType)
+         * 有界区间 ((-∞..b) or (-∞..b])	    upTo(C, BoundType)
+         */
+        // 等同于Range.closedOpen(2, 4)
+        Range<Integer> range = Range.range(2, BoundType.CLOSED, 4, BoundType.OPEN);
+        // [2..4)
+        println(range);
+
+        Range<Integer> downTo4AndOpen = Range.downTo(4, BoundType.OPEN);
+        // (4..+∞)
+        println(downTo4AndOpen);
+
+        Range<Integer> upTo4AndClose = Range.upTo(4, BoundType.CLOSED);
+        // (-∞..4]
+        println(upTo4AndClose);
+    }
+
+
+    /**
+     * 区间运算
+     */
+    private static void operations() {
+        /**
+         * 基本操作 contains
+         */
+        boolean contains = Range.closed(1, 3).contains(2);
+        // true
+        println(contains);
+
+        boolean contains1 = Range.closed(1, 3).contains(4);
+        // false
+        println(contains1);
+
+        boolean contains2 = Range.lessThan(5).contains(5);
+        // false
+        println(contains2);
+
+        boolean containsAll = Range.closed(1, 4).containsAll(Ints.asList(1, 2, 3));
+        // true
+        println(containsAll);
+
+
+
+        /**
+         * 查询运算
+         */
+        /**
+         * isEmpty()：判断是否为空区间。
+         */
+        boolean isEmpty = Range.closedOpen(4, 4).isEmpty();
+        // true
+        println(isEmpty);
+
+        boolean isEmpty2 = Range.openClosed(4, 4).isEmpty();
+        // true
+        println(isEmpty2);
+
+        boolean isEmpty3 = Range.closed(4, 4).isEmpty();
+        // false
+        println(isEmpty3);
+
+        // Range.open(4, 4).isEmpty();
+        // Range.open throws IllegalArgumentExceptio
+
+
+        /**
+         * 返回区间的端点值；如果区间没有对应的边界，抛出IllegalStateException；
+         */
+        Integer closedLowerEndpoint = Range.closed(3, 10).lowerEndpoint();
+        // 3
+        println(closedLowerEndpoint);
+
+        Integer openLowerEndpoint = Range.open(3, 10).lowerEndpoint();
+        // 3
+        println(openLowerEndpoint);
+
+        /**
+         * 返回区间边界类型，CLOSED或OPEN；如果区间没有对应的边界，抛出IllegalStateException
+         */
+        BoundType lowerBoundType = Range.closed(3, 10).lowerBoundType();
+        // CLOSED
+        println(lowerBoundType);
+
+        BoundType upperBoundType = Range.open(3, 10).upperBoundType();
+        // OPEN
+        println(upperBoundType);
+
+
+
+        /**
+         * 关系运算
+         */
+        /**
+         * 包含[enclose]
+         * 如果内区间的边界没有超出外区间的边界，则外区间包含内区间。包含判断的结果完全取决于区间端点的比较
+         */
+        // [1..4) 包含 (1..4)
+        boolean encloses = Range.closedOpen(1, 4).encloses(Range.open(1, 4));
+        // true
+        println(encloses);
+
+        /**
+         * 相连[isConnected]
+         * 判断区间是否是相连的, 等同于数学上的定义”两个区间的并集是连续集合的形式”
+         */
+        boolean closedOpen = Range.closed(3, 5).isConnected(Range.open(5, 10));
+        // true
+        println(closedOpen);
+
+        boolean openClose = Range.open(3, 5).isConnected(Range.open(5, 10));
+        // false
+        println(openClose);
+
+    }
+
+
+    private static void println(Object object) {
+        System.out.println(object.toString());
+    }
 }
