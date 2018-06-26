26ee9e661f65eff21cac8b39bed4a40cfda6465b
王宇轩 <vincent.yuxuan.wang@gmail.com>
2018-06-26 23:41:44 +0800

    EventBus事件总线

---
+++ src/main/java/ch11_event/EventBusExplained.java
@@ -0,0 +1,15 @@
+package ch11_event;
+
+
+/**
+ * 事件总线
+ *
+ * @author
+ * @create 2018-06-24 下午10:01
+ **/
+public class EventBusExplained {
+
+    public static void main(String[] args) {
+    }
+}
+
