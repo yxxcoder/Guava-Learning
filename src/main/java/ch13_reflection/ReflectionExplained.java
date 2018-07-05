e81270761f32284b7b4c207eb073e9f04d0983e1
yuxuan <vincent.yuxuan.wang@gmail.com>
2018-07-05 23:33:15 +0800

    TypeToken

--- src/main/java/ch13_reflection/ReflectionExplained.java
+++ src/main/java/ch13_reflection/ReflectionExplained.java
@@ -1,10 +1,21 @@
 package ch13_reflection;
 
+import com.google.common.reflect.TypeToken;
+
+import java.util.List;
+
 /**
  * Guava 的 Java 反射机制工具类
  *
- * @author
+ * @author yuxuan
  * @create 2018-07-04 下午9:39
  **/
 public class ReflectionExplained {
+    public static void main(String[] args) {
+        TypeToken<String> stringTok = TypeToken.of(String.class);
+        TypeToken<Integer> intTok = TypeToken.of(Integer.class);
+
+        TypeToken<List<String>> stringListTok = new TypeToken<List<String>>() {};
+    }
+
 }
